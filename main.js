882b726bb46be35f2f0e3dd1fab035b115336eae
Jeremias <abellojeremias@gmail.com>
2022-07-31 13:14:52 -0300

    se agrega desafio de JavaScript

---
+++ main.js
@@ -0,0 +1,14 @@
+//Utilizando While para hacer un bucle.
+
+let entrada = prompt("Ingresar datos correspondiente");
+//Repetimos hasta que se ingresa "ESC"
+while(entrada != "Jeremias Abello" ){
+   switch (entrada) {
+       case "ESC":
+           alert("¿QUIÉN SOS?");
+           break;
+   }
+   entrada = prompt("Ingresar datos correspondiente");
+
+console.log("Ingresar datos correspondiente")
+}
